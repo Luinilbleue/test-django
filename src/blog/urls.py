234d8ed0a779990a7a326c748781c51d6daf0435
charly <charly@agicia.com>
2021-03-07 11:43:31 +0100

    urls: setting up json views

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
 
 app_name = 'blog'
 
+router = routers.DefaultRouter()
+router.register('posts', views.AllPostsViewSet)
+
 urlpatterns = [
     path('', views.home, name="homepage"),
     path('list', views.list_page, name="posts_list_page"),
-    path('<slug:post>', views.single_post, name="single_post_page"),
+    path('post/<slug:post>', views.single_post, name="single_post_page"),
+    path('json_', include(router.urls))
 ]
